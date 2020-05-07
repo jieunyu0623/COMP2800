4a153323ab7c0dc7296fabfb12d877801f4a3f40
hihihippo <[zhang.chris89@gmail.com]>
2020-05-06 19:46:26 -0700

    script.js for chatbox

---
+++ script.js
@@ -0,0 +1,42 @@
+const socket = io('http://localhost:3000')
+const msgContainer = document.getElementById('msg-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+function getName() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("Users").doc(user.uid)
+            .onSnapshot(function (snap) {
+                document.getElementById("user_name").innerHTML = snap.data().name;
+            });
+    });
+}
+const name = getName();
+appendMessage('You joined');
+socket.emit('new-user', name);
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`);
+})
+//when users connects says person joined
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`);
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`);
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`);
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  msgContainer.append(messageElement);
+}
