a7700297ad9f9dab8c7693a0e68338a14d161540
hihihippo <[zhang.chris89@gmail.com]>
2020-05-07 15:46:03 -0700

    added a scripts with a getter setter and add activities

---
+++ scripts.js
@@ -0,0 +1,35 @@
+//this function will grab the array from the users the array is called arr
+function getActivities() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("Users").doc(user.uid)
+            .onSnapshot(function (snap) {
+                arr: firebase.firestore.FieldValue.arrayUnion( 'newItem' );
+                let x = snap.data().arr;
+                console.log(snap.data().arr);
+
+
+            });
+    });
+}
+//this function will remove the item to be removed for the users
+    function removeActivities(){
+        firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("Users").doc(user.uid)
+        .update({
+           
+            arr: firebase.firestore.FieldValue.arrayRemove("item to be removed")
+        
+        })
+    });
+}
+//array union ensures there are no repeats in our array so no 2 items will be the same.
+function addActivities(){
+        firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("Users").doc(user.uid)
+        .update({
+           
+            arr: firebase.firestore.FieldValue.arrayUnion("item to be added into array. ")
+        
+        })
+    });
+}
