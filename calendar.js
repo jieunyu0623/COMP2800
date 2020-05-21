e309165d4a4e60c10260de4f25d5468513bb5ff5
jieun <kibaco15@gmail.com>
2020-05-21 15:30:33 -0700

    testing2

--- calendar.js
+++ calendar.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     var api_url = 'https://www.zoho.com/calendar/api/v1'
     var auth = 'https://accounts.zoho.com/oauth/v2/auth'
-    var client_id = '1000.777P0YANPJB15P8L94539FMRZ2TKKH'
-    var client_secret = 'ebfccc0cc4cf5710fc60ef702628be67f2f8322406'
+    var client_id = '1000.8T025AE1A678IV0KM31TXHEEO3CM4H'
+    var client_secret = '49bf38d022248d1742d577612b7593c6795faa013f'
     var redirect_uri = 'https://comp2800-164b5.web.app'
     $.ajax({
-        url: auth + "?client_id=" + client_id + "&response_type=code&scope=AaaServer.profile.READ%2CAaaServer.profile.UPDATE&redirect_uri=" + redirect_uri,
+        url: auth + "?client_id=" + client_id + "&response_type=token&scope=AaaServer.profile.READ&redirect_uri=" + redirect_uri,
         type: "GET",
+        contentType: "application/json",
         success: function(result) {
             console.log(result);
         },
