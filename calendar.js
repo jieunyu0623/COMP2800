25e891ec50d88c5af5f68243264c1157729baa6d
jieun <kibaco15@gmail.com>
2020-05-21 14:07:51 -0700

    calendar api calls to get the page testing

--- calendar.js
+++ calendar.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+    var api_url = 'https://www.zoho.com/calendar/api/v1'
+    var auth = 'https://accounts.zoho.com/oauth/v2/auth'
+    var client_id = '1000.777P0YANPJB15P8L94539FMRZ2TKKH'
+    var client_secret = 'ebfccc0cc4cf5710fc60ef702628be67f2f8322406'
+    var redirect_uri = 'https://comp2800-164b5.web.app'
     $.ajax({
-        url: "https://www.zoho.com/calendar/api/v1",
+        url: auth + "?client_id=" + client_id + "&response_type=code&scope=AaaServer.profile.READ%2CAaaServer.profile.UPDATE&redirect_uri=" + redirect_uri,
         type: "GET",
         success: function(result) {
             console.log(result);
@@ -11,9 +16,5 @@ $(document).ready(function () {
     })
 })
 
-const api_url = '​https://calendar.zoho.com/api/v1/calendars/ebfccc0cc4cf5710fc60ef702628be67f2f8322406'
-const response = await fetch(api_url);
-const json = await response.json();
-console.log(json);
 
 
